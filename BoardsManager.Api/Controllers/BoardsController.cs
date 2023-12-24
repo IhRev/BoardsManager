2e5dac5ea107cadedd84f8a4d4919264a7050a09
preca <preca@PRECAST-29>
2023-12-24 17:29:22 +0100

    Add project files.

---
+++ BoardsManager.Api/Controllers/BoardsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BoardsManager.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BoardsController : ControllerBase
+    {
+        [HttpGet("cards")]
+        public async Task<ActionResult> Get([FromQuery]int projectId)
+        {
+            await Task.Delay(100);
+            return Ok();
+        }
+    }
+}
---
+++ BoardsManager.Core/DTO/CardDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace BoardsManager.Core.DTO
+{
+    public class CardDTO
+    {
+        public int Id { get; set; }
+
+        public string Type { get; set; } = null!;
+
+        public string State { get; set; } = null!;
+
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public string Executor { get; set; } = null!;
+
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        public int ProjectId { get; set; }
+    }
+}
